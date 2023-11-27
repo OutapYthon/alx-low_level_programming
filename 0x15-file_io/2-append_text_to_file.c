b296288b489601d1883936541b5c0b4242539abd
OutapYthon <badroutazarine1@gmail.com>
2023-11-27 05:06:35 -0800

    15

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fb = open(filename, O_WRONLY | O_APPEND);
 
 	if (fb == -1)
 		return (-1);
